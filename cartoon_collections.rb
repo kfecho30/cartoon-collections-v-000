1bf9fdbc9f734376450d01c9409676af36a8ba62
Kelsey Fecho <kfecho30@gmail.com>
2018-03-07 21:05:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,10 @@ def roll_call_dwarves(array)
   }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |arr|
+    arr.capitalize << !
+  end
 end
 
 def long_planeteer_calls# code an argument here
