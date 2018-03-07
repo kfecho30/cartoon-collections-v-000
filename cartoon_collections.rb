649ba40b8d6d77e9cd4eff118f5d58b11b2ffda5
Kelsey Fecho <kfecho30@gmail.com>
2018-03-07 21:20:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,7 @@ def find_the_cheese(array)
   found = ""
   array.each do |a|
     if cheese_types.include?(a)
-      found = a
-    else
-      found = nil
+      found << a
     end
   end
   found
